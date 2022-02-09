0650cfdd21b97fd8a7bcb3a5e3b520cc01120fae
Maybe Waffle <waffle.lapkin@gmail.com>
2022-02-09 03:42:15 +0300

    Write test text in a nicer way
    Somewhat surprisingly, default Rust string literals can be multiline.
    They are somewhat annoying because they include leading spaces (so you
    can't indent them), but at top level they are a lot nicer then
    concatenation via `concat!`.

--- f103/src/main.rs
+++ f103/src/main.rs
@@ -206,15 +206,14 @@ fn usb_poll<B: bus::UsbBus>(
     }
 }
 
-const LOREM: &[u8] = concat!(
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor\n",
-    "incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis\n",
-    "nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\n",
-    "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu\n",
-    "fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in\n",
-    "culpa qui officia deserunt mollit anim id est laborum.\n\n"
-)
-.as_bytes();
+const LOREM: &[u8] = b"\
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+culpa qui officia deserunt mollit anim id est laborum.\n
+";
 
 struct Keys<'l> {
     text: &'l [u8],
